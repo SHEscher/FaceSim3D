aef20655024257db8ab380632ecf77aafa62ab2e
simon.m.hofmann <simon.hofmann@cbs.mpg.de>
2024-11-05 22:15:07 +0100

    continue init

---
+++ code/facesim3d/__init__.py
@@ -0,0 +1,11 @@
+"""`facesim3d`: A Python package to reproduce the `FaceSim3D` study."""
+
+__author__ = "Simon M. Hofmann"
+__version__ = "0.0.0"
+__year__ = "2022-2024"
+
+import warnings
+
+import facesim3d.configs  # noqa: F401
+
+warnings.filterwarnings(action="ignore", category=FutureWarning)
---
+++ setup.py
@@ -0,0 +1,6 @@
+"""Setup file for the `facesim3d` package."""
+
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup()
